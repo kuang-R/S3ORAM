df512980c93debaf67635b4360ab7a544a82a7f1
comeonsunny <2286323940@qq.com>
2022-01-21 16:54:38 +0800

    Update ClientS3ORAM.cpp
    the access operation has been completed.

--- S3ORAM/ClientS3ORAM.cpp
+++ S3ORAM/ClientS3ORAM.cpp
@@ -218,6 +218,9 @@ int ClientS3ORAM::sendORAMTree()
  */  
 int ClientS3ORAM::access(TYPE_INDEX blockID)
 {
+	auto start_all = time_now;
+	auto end_all = time_now;
+	start_all = time_now;
 	S3ORAM ORAM;
 	cout << "================================================================" << endl;
 	cout << "STARTING ACCESS OPERATION FOR BLOCK-" << blockID + 1 <<endl; 
@@ -239,7 +242,8 @@ int ClientS3ORAM::access(TYPE_INDEX blockID)
     
     
 	// 3. send to server & receive the answer
-    
+    auto start = time_now;
+    auto end = time_now;
     start = time_now;
     {
         memcpy(stash_index_buffer_out, &stash_index, sizeof(TYPE_INDEX));
@@ -248,172 +252,59 @@ int ClientS3ORAM::access(TYPE_INDEX blockID)
     }
     
     end = time_now;
+	exp_logs[0] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
     cout<< "	[ClientJumpORAM] All Blocks in Stash Retrieved in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()<< " ns"<<endl;
 
 	
     // 4. Read the block client wants and swap it with the block in data cache randomly
+	TYPE_INDEX real_block_index_stash = physicalID / STEP;
+	TYPE_INDEX block_index_cache = rand() % DATA_CACHE;
 	if (physicalID < NStore){
-		
-	}else{
-		TYPE_INDEX real_block_index_stash = physicalID / STEP;
-		TYPE_INDEX block_index_cache = rand() % DATA_CACHE;
 		TYPE_DATA *temp_mem = new TYPE_DATA[DATA_CHUNKS];
 		memcpy((void *)temp_mem, (void *)(stash_buffer_in + real_block_index_stash * BLOCK_SIZE), BLOCK_SIZE);
 		memcpy((void *)(stash_buffer_in + real_block_index_stash * BLOCK_SIZE), (void *)(*data_cache)[block_index_cache], BLOCK_SIZE);
 		memcpy((void *)(*data_cache)[block_index_cache], (void*)temp_mem, BLOCK_SIZE);
-	}
- 
-	start = time_now;
-	ORAM.simpleRecover(retrievedShare, recoveredBlock);
-	end = time_now;
-	cout<< "	[ClientS3ORAM] Recovery Done in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()<< " ns"<<endl;
-	exp_logs[3] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
-	
-    
-    cout << "	[ClientS3ORAM] Block-" << recoveredBlock[0] <<" is Retrieved" <<endl;
-    if (recoveredBlock[0] == blockID)
-        cout << "	[ClientS3ORAM] SUCCESS!!!!!!" << endl;
-    else
-        cout << "	[ClientS3ORAM] ERROR!!!!!!!!" << endl;
+		delete temp_mem;
 		
-    assert(recoveredBlock[0] == blockID && "ERROR: RECEIEVED BLOCK IS NOT CORRECT!!!!!!");
+	}else{
+		// do nothing 
+	}
+	
+	// rewrite operation without any encryption (this time modify it later)
+	
+	
 	
     
     // 6. update position map
     
-    TYPE_INDEX fullPathIdx[H+1];
-    ORAM.getFullPathIdx(fullPathIdx,pathID);
-    this->metaData[fullPathIdx[pos_map[blockID].pathIdx / BUCKET_SIZE ]][pos_map[blockID].pathIdx % BUCKET_SIZE] = 0;
-    
-    // 6.1. assign to random path
-    pos_map[blockID].pathID = Utils::RandBound(N_leaf)+(N_leaf-1);
-    pos_map[blockID].pathIdx = numRead;
-    this->metaData[0][numRead] = blockID;
+    if (physicalID < NStore){
+		//update the information in position map 
+		std::swap((*pos_map)[blockID], (*pos_map)[NStore + block_index_cache]);
+	}
     
     
-    // 7. create new shares for the retrived block, 
-    TYPE_DATA chunkShares[NUM_SERVERS];
-    
-    for(int u = 0 ; u < DATA_CHUNKS; u++ )
-    {
-        ORAM.createShares(recoveredBlock[u], chunkShares);
-        
-        for(int k = 0; k < NUM_SERVERS; k++) 
-        {
-                memcpy(&block_buffer_out[k][u*sizeof(TYPE_DATA)], &chunkShares[k], sizeof(TYPE_DATA));
-        }
-    }
-    for ( int k = 0 ; k < NUM_SERVERS; k++)
-    {
-        memcpy(&block_buffer_out[k][DATA_CHUNKS*sizeof(TYPE_DATA)], &numRead, sizeof(TYPE_DATA));
-    }
 	// 8. upload the share to numRead-th slot in root bucket
-    for(TYPE_INDEX k = 0; k < NUM_SERVERS; k++) 
-    {
-//		thread_args[k] = struct_socket(SERVER_ADDR[k]+ ":" + SERVER_PORT[k*NUM_SERVERS+k], block_buffer_out[k], sizeof(TYPE_DATA)*DATA_CHUNKS+sizeof(TYPE_INDEX), NULL, 0, CMD_SEND_BLOCK,NULL);
-		thread_args[k] = struct_socket(SERVER_ADDR[k]+ ":" + std::to_string(SERVER_PORT+k*NUM_SERVERS+k), block_buffer_out[k], sizeof(TYPE_DATA)*DATA_CHUNKS+sizeof(TYPE_INDEX), NULL, 0, CMD_SEND_BLOCK,NULL);
-
-		pthread_create(&thread_sockets[k], NULL, &ClientS3ORAM::thread_socket_func, (void*)&thread_args[k]);
-    }
-    
-	this->numRead = (this->numRead+1)%EVICT_RATE;
-    cout << "	[ClientS3ORAM] Number of Read = " << this->numRead <<endl;
-    
-    for (int i = 0; i < NUM_SERVERS; i++)
-    {
-        pthread_join(thread_sockets[i], NULL);
-        cout << "	[ClientS3ORAM] Block upload completed!" << endl;
-    }
-    
+	start = time_now;
+	sendNrecv(SERVER_ADDR[0]+ ":" + std::to_string(SERVER_PORT), (unsigned char *)&stash_index, sizeof(stash_index), nullptr, 0, CMD_SEND_BLOCK);
+	sendNrecv(SERVER_ADDR[0]+ ":" + std::to_string(SERVER_PORT), stash_buffer_in, STASH * DATA_CHUNKS * sizeof(TYPE_DATA),nullptr, 0, CMD_SEND_BLOCK);
+	end = time_now;
+	exp_logs[1] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+    cout<< "	[ClientJumpORAM] All Blocks in Stash has been sent to server in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()<< " ns"<<endl;
 	cout << "================================================================" << endl;
 	cout << "ACCESS OPERATION FOR BLOCK-" << blockID << " COMPLETED." << endl; 
 	cout << "================================================================" << endl;
 	
-    // 9. Perform eviction
-    if(this->numRead == 0)
-    {
-        
-        cout << "================================================================" << endl;
-		cout << "STARTING EVICTION-" << this->numEvict+1 <<endl;
-		cout << "================================================================" << endl;
-        
-        // 9.1. create permutation matrices
-		for(TYPE_INDEX i = 0 ; i < H+1; i++)
-        {
-			memset(this->evictMatrix[i], 0, evictMatSize*sizeof(TYPE_DATA));
-		}
-
-		start = time_now;
-        this->getEvictMatrix(this->evictMatrix,numEvict);
-		end = time_now;
-		cout<< "	[ClientS3ORAM] Evict Matrix Created in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()<< " ns"<<endl;
-		exp_logs[5] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
-		
-		
-		// 9.2. create shares of  permutation matrices 
-		cout<< "	[ClientS3ORAM] Sharing Evict Matrix..." << endl;
-		
-		boost::progress_display show_progress((H+1)*evictMatSize);
-        TYPE_DATA matrixShares[NUM_SERVERS];
-        start = time_now;
-        for (TYPE_INDEX i = 0; i < H+1; ++i) 
-        {
-            for (TYPE_INDEX j = 0; j < evictMatSize; ++j)
-            {
-                ORAM.createShares(this->evictMatrix[i][j], matrixShares);
-                for (int k = 0; k < NUM_SERVERS; k++) 
-                {   
-                    this->sharedMatrix[k][i][j] = matrixShares[k];
-                }
-				++show_progress;
-            }
-        }
-        end = time_now;
-        cout<< "	[ClientS3ORAM] Shared Matrix Created in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()<< " ns"<<endl;
-        exp_logs[6] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
-        
-        // 9.3. send permutation matrices to servers
-        start = time_now;
-        for (int i = 0; i < NUM_SERVERS; i++)
-        {
-            for (TYPE_INDEX y = 0 ; y < H+1; y++)
-            {
-                memcpy(&evict_buffer_out[i][y*evictMatSize*sizeof(TYPE_DATA)], &this->sharedMatrix[i][y][0], evictMatSize*sizeof(TYPE_DATA));
-            }
-            memcpy(&evict_buffer_out[i][(H+1)*evictMatSize*sizeof(TYPE_DATA)], &numEvict, sizeof(TYPE_INDEX));
-            
-			thread_args[i] = struct_socket(SERVER_ADDR[i]+ ":" + std::to_string(SERVER_PORT+i*NUM_SERVERS+i), evict_buffer_out[i], (H+1)*evictMatSize*sizeof(TYPE_DATA) + sizeof(TYPE_INDEX), NULL,0, CMD_SEND_EVICT,  NULL);
-//            thread_args[i] = struct_socket(SERVER_ADDR[i]+ ":" + SERVER_PORT[i*NUM_SERVERS+i], evict_buffer_out[i], (H+1)*evictMatSize*sizeof(TYPE_DATA) + sizeof(TYPE_INDEX), NULL,0, CMD_SEND_EVICT,  NULL);
-            pthread_create(&thread_sockets[i], NULL, &ClientS3ORAM::thread_socket_func, (void*)&thread_args[i]);
-        }
-			
-        for (int i = 0; i < NUM_SERVERS; i++)
-        {
-            pthread_join(thread_sockets[i], NULL);
-        }
-        end = time_now;
-        cout<< "	[ClientS3ORAM] Eviction DONE in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count()<< " ns"<<endl;
-        
-        exp_logs[8] = thread_max;
-        thread_max = 0;
-		
-		cout << "================================================================" << endl;
-		cout << "EVICTION-" << this->numEvict+1 << " COMPLETED" << endl;
-		cout << "================================================================" << endl;
-        
-        this->numEvict = (numEvict+1) % N_leaf;
-    }
-    // 11. store local info to disk
+    // 9. store local info to disk
 	FILE* local_data = NULL;
 	if((local_data = fopen(clientTempPath.c_str(),"wb+")) == NULL){
-		cout<< "	[ClientS3ORAM] File Cannot be Opened!!" <<endl;
+		cout<< "	[ClientJumpORAM] File Cannot be Opened!!" <<endl;
 		exit(0);
 	}
-	fwrite(this->pos_map, 1, (NUM_BLOCK+1)*sizeof(TYPE_POS_MAP), local_data);
-	fwrite(&this->numEvict, sizeof(this->numEvict), 1, local_data);
-	fwrite(&this->numRead, sizeof(this->numRead), 1, local_data);
+	fwrite(this->pos_map, 1,(*pos_map).size()*sizeof(TYPE_INDEX), local_data);
 	fclose(local_data);
-     
+	end_all = time_now;
+	exp_logs[3] = std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count();
+	cout<< "	[ClientJumpORAM] executing operation of access  in " << std::chrono::duration_cast<std::chrono::nanoseconds>(end_all-start_all).count()<< " ns"<<endl;
 	// 12. write log
 	Utils::write_list_to_file(to_string(HEIGHT)+"_" + to_string(BLOCK_SIZE)+"_client_" + timestamp + ".txt",logDir, exp_logs, 9);
 	memset(exp_logs, 0, sizeof(unsigned long int)*9);
